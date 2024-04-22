bace437869c1f06e937357a8bf76f22841f6b4de
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-22 19:08:56 +0200

    refactor: add startServer method

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,33 @@ const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connectToMongo = require("./db");
-connectToMongo();
+const { connectToMongo, disconnectFromMongo} = require("./db");
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const startServer = async () => {
+  try {
+    await connectToMongo();
+    // set port, listen for requests
+    const PORT = process.env.PORT || 8080;
+    
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+    // Server Shutting down
+    process.on("SIGINT", async () => {
+      console.log("Shutting down the server...");
+      await disconnectFromMongo();
+
+      server.close(() => {
+        console.log("Server successfully shut down.");
+        process.exit(0);
+      });
+    });
+    
+  } catch (error) {
+    console.error("Error starting server:", error);
+    disconnectFromMongo();
+  }
+};
+
+startServer();
