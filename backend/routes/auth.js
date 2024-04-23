c8c2c3a6af217064b8a155492a70f748a958cae3
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-23 22:12:41 +0200

    Add signOut route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { signUp, signIn } = require('../controllers/auth');
+const { signUp, signIn, signOut } = require('../controllers/auth');
 
 router.post('/signup', signUp);
 router.post('/login', signIn);
+router.post('/logout', signOut);
 
 module.exports = router;
