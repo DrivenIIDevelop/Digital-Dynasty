6f05293230f49268f583608cb2ef747e4c09dc55
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-30 16:00:35 +0200

    Add the authenticateUser middleware to the users routes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getUser, updateUser } = require('../controllers/user');
+const authenticateUser = require('../middlewares/authenticateUser');
 
 
-router.get('/profile/:userId', getUser);
-router.put('/profile/:userId', updateUser);
+router.get('/profile/:userId', authenticateUser, getUser);
+router.put('/profile/:userId', authenticateUser, updateUser);
 
 module.exports = router;
